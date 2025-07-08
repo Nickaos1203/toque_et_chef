62a69c06aaaf66f397535731f880fa6e9d6a7c91
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-07-08 16:47:41 +0200

    add other spiders

--- web_scraping/web_scraping/spiders/dishes_spider.py
+++ web_scraping/web_scraping/spiders/dishes_spider.py
@@ -4,19 +4,35 @@ import scrapy
 import re
 
 class MarmiteSpider(scrapy.Spider):
-    name= "appetizer_spider"
+    name= "dishes_spider"
     allowed_domains = ["marmiton.org"]
-    start_urls = ["https://www.marmiton.org/recettes?page=" + str(x) for x in range(1, 400)]
+    start_urls = ["https://www.marmiton.org/recettes?type=platprincipal&page=" + str(x) for x in range(1, 150)]
+
 
     def parse(self, response):
-        recipe_links = response.css("div.recipe-card a.recipe-card-link::attr(href)").getall()
+        recipe_cards = response.css("div.recipe-card")
 
-        for link in recipe_links:
-            yield response.follow(link, callback=self.parse_recipe)
+        for card in recipe_cards:
+            link = card.css("a.recipe-card-link::attr(href)").get()
+            image_url = card.css("a.recipe-card-link div.recipe-card__picture img::attr(data-src)").get()
+
+            if link:
+                yield response.follow(
+                    link,
+                    callback=self.parse_recipe,
+                    cb_kwargs={'image_url': image_url}
+                )
 
     
-    def parse_recipe(self, response):
+    def parse_recipe(self, response, image_url):
         title = response.css("div.recipe-header__title div.main-title h1::text").get()
+
+        categories = response.css("div.recipe-header__title div.main-title div.show-more__modal div.modal__tags span.modal__tag *::text").getall()
+        season = categories[0]
+        category = categories[1]
+
+        tags = response.css("div.recipe-header__title div.main-title div.show-more__modal div.modal__tags a.modal__tag *::text").getall()
+
         duration = response.css("div.recipe-primary div.recipe-primary__item span::text").get()
         level = response.css("div.recipe-primary div.recipe-primary__item span::text")[1].get()
         cost = response.css("div.recipe-primary div.recipe-primary__item span::text")[2].get() 
@@ -34,4 +50,20 @@ class MarmiteSpider(scrapy.Spider):
 
         steps = response.css("div.recipe-preparation div.recipe-step-list div.recipe-step-list__container p *::text").getall()
         author = response.css("div.recipe-author-note div.recipe-author-note__head div span.recipe-author-note__author-name::text").get()
+
+        yield {
+            'title':title,
+            'image': image_url,
+            'season': season,
+            'category': category,
+            'tags': tags,
+            'duration': duration,
+            'level': level,
+            'cost': cost,
+            'number': number,
+            'persons_number': persons_number,
+            'ingredients': ingredients,
+            'steps': steps,
+            'author': author,
+        }
 
