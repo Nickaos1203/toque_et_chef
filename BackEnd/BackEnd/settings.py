863c9b8999ea35a19338f3313ce6e04630bded8c
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-07-12 23:15:06 +0200

    user app init

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #apps
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
