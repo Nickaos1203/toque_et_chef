a8fd01f21aea3cf820fad137f8299e575e581e93
Nickaos1203 <nicolas.guillon95@yahoo.com>
2025-07-15 00:12:55 +0200

    add crud recipe

--- django_app/recipes/serializers.py
+++ django_app/recipes/serializers.py
@@ -2,3 +2,12 @@
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
+from rest_framework import serializers
+from .models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Recipe
+        fields = '__all__'
